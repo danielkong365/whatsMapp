78498ce1c39cc3971a867b78a9ef1dae998be375
Daniel Kong <danielkong365@gmail.com>
2020-03-28 15:17:33 -0400

    Add message replies

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from app import app
+from flask import request
+from twilio.twiml.messaging_response import MessagingReponse
 
 @app.route('/')
 @app.route('/index')
-
 def index():
 	return "Welcome to WhatsMapp!"
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
 
