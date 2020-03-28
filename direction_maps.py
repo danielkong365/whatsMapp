e8b69fdac8b5cd26816151bf944527c3487c2d37
Julie Wang <julie.wang@emory.edu>
2020-03-28 17:41:01 -0400

    Create direction_maps.py

---
+++ direction_maps.py
@@ -0,0 +1,14 @@
+### pip install -U googlemaps
+
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key=os.getenv('gmapsAPI'))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+print(directions_result)
